62cc3b1470279614c58b598abf5c04f39c467c37
Karun Dawadi <43080986+karundawadi@users.noreply.github.com>
2023-03-06 22:57:14 -0800

    Revert "Update site, readme , navigation"
    This reverts commit e0f74b52d34dee08b182c645ab1f3e1061c14137.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,25 +12,11 @@ import { VideoComponent } from './components/home/video/video.component';
 import { CoverpageComponent } from './components/home/coverpage/coverpage.component';
 import { FormComponent } from './components/form/form.component';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-
-// Update this if any routes are added in the future
-const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'menu', component: MenuComponent},
-  {path: 'order', component: FormComponent},
-  {path: 'contact', component: ContactComponent}
-];
 
 @NgModule({
   imports:[
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(routes, {
-      initialNavigation: 'enabledNonBlocking',
-      useHash: true
-    })
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
@@ -48,7 +34,6 @@ const routes: Routes = [
     provide: LocationStrategy, 
     useClass: PathLocationStrategy
   }],
-  bootstrap: [AppComponent],
-  exports: [RouterModule]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
